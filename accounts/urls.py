a7285315426556a3bd4b91ef071e58aaa4e53112
Dragos Mitroi <dragos.mitroi.1985@gmail.com>
2023-09-12 22:42:19 +0300

    user registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    
+    
    
 ]
